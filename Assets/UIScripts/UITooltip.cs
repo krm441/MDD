10b296a57f1d1fdabda1a72b4248db23b18bf723
krm441 <kamal.raed.moustafa@gmail.com>
2025-07-05 06:19:05 +0200

    fix: tooltip: better padding and logic

--- Assets/UIScripts/UITooltip.cs
+++ Assets/UIScripts/UITooltip.cs
@@ -6,17 +6,22 @@ public class UITooltip : MonoBehaviour
     public static UITooltip Instance;
     public CanvasGroup canvasGroup;
     public Text tooltipText;
+    public Image image;
     public RectTransform background;
 
-    private void Awake() => Instance = this;
+    private void Awake()
+    {
+        Instance = this; Hide();
+        //Show("Some nice tooltip", new Vector2(500, 200)); DEBUG
+    }
 
     public void Show(string text, Vector2 screenPos)
     {
         tooltipText.text = text;
-        background.sizeDelta = new Vector2(tooltipText.preferredWidth + 20, tooltipText.preferredHeight + 20);
+        background.sizeDelta = new Vector2(tooltipText.preferredWidth + 8, tooltipText.preferredHeight + 8);
         transform.position = screenPos;
-        canvasGroup.alpha = 1;
+        gameObject.SetActive(true);
     }
 
-    public void Hide() => canvasGroup.alpha = 0;
+    public void Hide() => gameObject.SetActive(false);
 }
