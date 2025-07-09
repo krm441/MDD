7efe54ea60c8332bcdd7b80a292f2a5fc17439f4
krm441 <kamal.raed.moustafa@gmail.com>
2025-07-09 03:22:11 +0200

    feat: BTs: added simple classic BT node and Sequence

---
+++ Assets/GameAi/BehaviorTree.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum BTState { Success, Failure, Running }
+
+public abstract class BTNode
+{
+    public abstract BTState Tick();
+}
+public class Sequence : BTNode
+{
+    private readonly List<BTNode> children;
+
+    public Sequence(params BTNode[] nodes) => children = new List<BTNode>(nodes);
+
+    public override BTState Tick()
+    {
+        foreach (var child in children)
+        {
+            var result = child.Tick();
+            if (result != BTState.Success)
+                return result; // Failure or Running
+        }
+        return BTState.Success;
+    }
+}
+
